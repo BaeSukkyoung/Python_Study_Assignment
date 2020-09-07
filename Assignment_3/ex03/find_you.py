6d0afa54ca21373b1b6b5414f92bb2ceea582e1d
BaeSukkyoung <61176230+BaeSukkyoung@users.noreply.github.com>
2020-09-07 21:15:57 +0900

    Add files via upload

---
+++ Assignment_3/ex03/find_you.py
@@ -0,0 +1,8 @@
+bag = [2, 4, 6, 8, 10]
+
+i = 10
+
+if i is in bag:
+    print('Yes')
+else 
+    print('No')
---
+++ Assignment_3/ex05/Mersenne.py
@@ -0,0 +1,4 @@
+if (2**(n-1) for n in range(65)) & (2**65 % 2**(n-1) == 0)
+    print('Yes')
+else
+    print('No')
